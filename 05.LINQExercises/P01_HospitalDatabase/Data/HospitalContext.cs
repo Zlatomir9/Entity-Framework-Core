1a54848540c5d38cdf77d1248914559ae92a012a
Zlatomir9 <71620439+Zlatomir9@users.noreply.github.com>
2021-03-06 14:14:46 +0200

    CodeFirst Exercises

---
+++ 05.LINQExercises/P01_HospitalDatabase/Data/HospitalContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data.Models;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext()
+        { }
+
+        public HospitalContext(DbContextOptions options)
+            : base(options)
+        { }
+
+        public DbSet<Diagnose> Diagnoses { get; set; }
+
+        public DbSet<Medicament> Medicaments { get; set; }
+
+        public DbSet<Patient> Patients { get; set; }
+
+        public DbSet<PatientMedicament> PatientMedicaments { get; set; }
+
+        public DbSet<Visitation> Visitations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;DataBase=Hospital;Integrated Security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PatientMedicament>()
+                .HasKey(x => new { x.PatientId, x.MedicamentId });
+        }
+    }
+}
---
+++ 05.LINQExercises/P01_HospitalDatabase/Data/Models/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Specialty { get; set; }
+
+        public ICollection<Visitation> Visitations { get; set; }
+    }
+}
