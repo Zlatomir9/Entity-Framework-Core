5f79f5a4de7a0f8b5637fc6070500ca03da17a18
Zlatomir9 <71620439+Zlatomir9@users.noreply.github.com>
2021-03-07 07:49:01 +0200

    CodeFirst Exercises

---
+++ 05.LINQExercises/P03_SalesDatabase/Data/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P03_SalesDatabase.Data.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        [Column(TypeName="nvarchar(100)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "varchar(80)")]
+        public string Email { get; set; }
+
+        public string CreditCardNumber { get; set; }
+
+        public ICollection<Sale> Sales { get; set; }
+    }
+}
---
+++ 05.LINQExercises/P03_SalesDatabase/Data/Models/Store.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P03_SalesDatabase.Data.Models
+{
+    public class Store
+    {
+        public int StoreId { get; set; }
+
+        [Column(TypeName = "nvarchar(80)")]
+        public string Name { get; set; }
+
+        public ICollection<Sale> Sales { get; set; }
+    }
+}
