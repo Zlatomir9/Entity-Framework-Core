d6d1371a18e1997e3e9f21b1e607b4cd469eeaee
Zlatomir9 <71620439+Zlatomir9@users.noreply.github.com>
2021-03-18 07:13:36 +0200

    JSON Processing Exercise

---
+++ 08.JSON Processing/ProductShop/DataTransferObjects/CategoryProductsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductShop.DataTransferObjects
+{
+    public class CategoryProductsModel
+    {
+        public int ProductId { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
