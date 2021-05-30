f3ee8f0381e04b63ed7a2b5abb50a622ccae93aa
Zlatomir9 <71620439+Zlatomir9@users.noreply.github.com>
2021-05-30 17:33:45 +0300

    Final Exam

---
+++ 11.Exam/TeisterMask/TeisterMask/DataProcessor/ExportDto/ProjectXmlExportModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace TeisterMask.DataProcessor.ExportDto
+{
+    [XmlType("Project")]
+    public class ProjectXmlExportModel
+    {
+        [XmlAttribute("TasksCount")]
+        public int TasksCount { get; set; }
+
+        [Required]
+        public string ProjectName { get; set; }
+
+        [Required]
+        public string HasEndDate { get; set; }
+
+        [XmlArray("Tasks")]
+        public TaskXmlExportModel[] Tasks { get; set; }
+    }
+}
---
+++ 11.Exam/TeisterMask/TeisterMask/DataProcessor/Serializer.cs
@@ -0,0 +1,76 @@
+﻿namespace TeisterMask.DataProcessor
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Serialization;
+    using Data;
+    using Newtonsoft.Json;
+    using TeisterMask.Data.Models.Enums;
+    using TeisterMask.DataProcessor.ExportDto;
+    using Formatting = Newtonsoft.Json.Formatting;
+
+    public class Serializer
+    {
+        public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
+        {
+            var projects = context.Projects
+                .Where(x => x.Tasks.Any())
+                .ToArray()
+                .Select(x => new ProjectXmlExportModel
+                {
+                    TasksCount = x.Tasks.Count,
+                    ProjectName = x.Name,
+                    HasEndDate = x.DueDate == null ? "No" : "Yes",
+                    Tasks = x.Tasks.Select(t => new TaskXmlExportModel
+                    {
+                        Name = t.Name,
+                        Label = Enum.GetName(typeof(LabelType), t.LabelType)
+                    })
+                    .OrderBy(x => x.Name)
+                    .ToArray()
+                })
+                .OrderByDescending(x => x.Tasks.Length)
+                .ThenBy(x => x.ProjectName)
+                .ToArray();
+
+            var xmlSerializer = new XmlSerializer(typeof(ProjectXmlExportModel[]), new XmlRootAttribute("Projects"));
+
+            var textWriter = new StringWriter();
+            var ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+            xmlSerializer.Serialize(textWriter, projects, ns);
+
+            return textWriter.ToString();
+        }
+
+        public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
+        {
+            var result = context.Employees
+                .Where(x => x.EmployeesTasks.Any(t => t.Task.OpenDate >= date))
+                .ToArray()
+                .Select(x => new
+                {
+                    x.Username,
+                    Tasks = x.EmployeesTasks.Where(t => t.Task.OpenDate >= date)
+                    .ToArray()
+                    .Select(x => new
+                    {
+                        TaskName = x.Task.Name,
+                        OpenDate = x.Task.OpenDate.ToString("d", CultureInfo.InvariantCulture),
+                        DueDate = x.Task.DueDate.ToString("d", CultureInfo.InvariantCulture),
+                        LabelType = Enum.GetName(typeof(LabelType), x.Task.LabelType),
+                        ExecutionType = Enum.GetName(typeof(ExecutionType), x.Task.ExecutionType)
+                    })
+                    .OrderByDescending(x => DateTime.Parse(x.DueDate, CultureInfo.InvariantCulture)).ThenBy(x => x.TaskName).ToList()
+                })
+                .OrderByDescending(x => x.Tasks.Count)
+                .ThenBy(x => x.Username)
+                .Take(10)
+                .ToList();
+
+            return JsonConvert.SerializeObject(result, Formatting.Indented);
+        }
+    }
+}
